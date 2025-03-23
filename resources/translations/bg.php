2522d30dc53d64ab9fbaf2318644ee9e525886bb
Anatoly Nekhay <afenric@gmail.com>
2025-03-23 15:09:01 +0100

    v1.2

--- resources/translations/bg.php
+++ resources/translations/bg.php
@@ -6,5 +6,5 @@ use Sunrise\Recaptcha\Dictionary\ErrorMessage;
 
 return [
     ErrorMessage::CHALLENGE_FAILED => 'Сигурни ли си, че не си робот?',
-    ErrorMessage::MISSING_OR_EMPTY_HEADER => 'Заглавката за заявката "{{ header_name }}" липсва или е празна.',
+    ErrorMessage::MISSING_OR_EMPTY_HEADER => 'Заглавката на заявката "{{ header_name }}" липсва или е празна.',
 ];
