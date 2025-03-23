9706ec914d69a5f44d004bcaa2f3c80c7ee46e5a
Anatoly Nekhay <afenric@gmail.com>
2025-03-23 15:15:26 +0100

    v1.2

--- resources/translations/sr.php
+++ resources/translations/sr.php
@@ -6,5 +6,5 @@ use Sunrise\Recaptcha\Dictionary\ErrorMessage;
 
 return [
     ErrorMessage::CHALLENGE_FAILED => 'Da li ste sigurni da niste robot?',
-    ErrorMessage::MISSING_OR_EMPTY_HEADER => 'Заглавље захтева "{{ header_name }}" недостаје или је празно.',
+    ErrorMessage::MISSING_OR_EMPTY_HEADER => 'Zaglavlje zahteva "{{ header_name }}" nedostaje ili je prazno.',
 ];
